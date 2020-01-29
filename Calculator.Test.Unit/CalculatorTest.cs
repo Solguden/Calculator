6a89bb4809a20630d0661493d2bce194c3abe0c5
JØ <201700290@post.au.dk>
2020-01-29 16:38:09 +0100

    xd

---
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+            uut = new Calculator();
+        }
+
+        [Test]
+        public void Add_2and2_Return4()
+        {
+            //Act
+            var check = uut.Add(2, 2);
+
+            //Assert
+            Assert.That(check, Is.EqualTo(4));
+        }
+
+    }
+}
