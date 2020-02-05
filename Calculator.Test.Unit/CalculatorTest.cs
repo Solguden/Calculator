f92f5a1b5dc39e0fdd8415ec681b240f13330171
Jacob <Jacob.svoldgaard93@gmail.com>
2020-02-05 13:37:46 +0100

    Tests for add og subtract

--- Calculator.Test.Unit/CalculatorTest.cs
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -30,5 +30,38 @@ namespace Calculator.Test.Unit
             Assert.That(check, Is.EqualTo(4));
         }
 
+        [TestCase(1, 2, 3)]
+        [TestCase(5,5, 10)]
+        [TestCase(5, 0, 5)]
+        [TestCase(0, 10, 10)]
+        [TestCase(10,-10, 0)]
+        [TestCase(-20, 20, 0)]
+        [TestCase(-5, -5, -10)]
+        public void Add(int integer1, int integer2, int result)
+        {
+            // Act
+            var check = uut.Add(integer1, integer2);
+
+            // Assert
+            Assert.That(check, Is.EqualTo(result));
+        }
+
+        [TestCase(5, 2, 3)]
+        [TestCase(5, 5, 0)]
+        [TestCase(10, 0, 10)]
+        [TestCase(5, 10, -5)]
+        [TestCase(0, 10, -10)]
+        [TestCase(10, -10, 20)]
+        [TestCase(-20, 20, -40)]
+        [TestCase(-5, -5, 0)]
+        public void Subtract(int integer1, int integer2, int result)
+        {
+            // Act
+            var check = uut.Subtract(integer1, integer2);
+
+            // Assert
+            Assert.That(check, Is.EqualTo(result));
+        }
+
     }
 }
